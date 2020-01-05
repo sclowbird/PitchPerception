dd5c08f3fecdaae898bff7cf41e58ab31918ce3d
sclowbird <gregor.hlinka@gmail.com>
2020-01-05 14:46:22 +0100

    Added descriptions of different audio features.

--- src/components/PitchPerception.vue
+++ src/components/PitchPerception.vue
@@ -14,54 +14,108 @@
       </b-form-select>
     </div>
     <div id="audioFeatureDropDown" v-if="audioFeatures.length > 0">
-      <b-form-select v-model="selectedAf" :options="featureDropDown" size="sm" class="mb-3">
+      <b-form-select
+        v-model="selectedAf"
+        :options="featureDropDown"
+        size="sm"
+        class="mb-3"
+      >
         <template v-slot:first>
           <option :value="null" disabled>Please select an audio feature</option>
         </template>
       </b-form-select>
       <div id="trend-diagram-container" v-if="selectedAf !== null">
         <div id="description" v-if="featureDropDown[selectedAf] != 'duration'">
-          <b-button v-b-modal.modal-1>What does {{ featureDropDown[selectedAf] }} mean?</b-button>
-          <b-modal hide-footer id="modal-1" :title="featureDropDown[selectedAf]">
-            <div id="danceability" v-if="featureDropDown[selectedAf] == 'danceability'">
-              <p
-                class="my-4"
-              >Danceability describes how suitable a track is for dancing based on a combination of musical elements including tempo, rhythm stability, beat strength, and overall regularity. A value of 0.0 is least danceable and 1.0 is most danceable.</p>
+          <b-button v-b-modal.modal-1
+            >What does {{ featureDropDown[selectedAf] }} mean?</b-button
+          >
+          <b-modal
+            hide-footer
+            id="modal-1"
+            :title="featureDropDown[selectedAf]"
+          >
+            <div
+              id="danceability"
+              v-if="featureDropDown[selectedAf] == 'danceability'"
+            >
+              <p class="my-4">
+                Danceability describes how suitable a track is for dancing based
+                on a combination of musical elements including tempo, rhythm
+                stability, beat strength, and overall regularity. A value of 0.0
+                is least danceable and 1.0 is most danceable.
+              </p>
             </div>
             <div id="energy" v-if="featureDropDown[selectedAf] == 'energy'">
-              <p
-                class="my-4"
-              >Energy is a measure from 0.0 to 1.0 and represents a perceptual measure of intensity and activity. Typically, energetic tracks feel fast, loud, and noisy. For example, death metal has high energy, while a Bach prelude scores low on the scale. Perceptual features contributing to this attribute include dynamic range, perceived loudness, timbre, onset rate, and general entropy.</p>
+              <p class="my-4">
+                Energy is a measure from 0.0 to 1.0 and represents a perceptual
+                measure of intensity and activity. Typically, energetic tracks
+                feel fast, loud, and noisy. For example, death metal has high
+                energy, while a Bach prelude scores low on the scale. Perceptual
+                features contributing to this attribute include dynamic range,
+                perceived loudness, timbre, onset rate, and general entropy.
+              </p>
             </div>
-            <div id="speechiness" v-if="featureDropDown[selectedAf] == 'speechiness'">
-              <p
-                class="my-4"
-              >Speechiness detects the presence of spoken words in a track. The more exclusively speech-like the recording (e.g. talk show, audio book, poetry), the closer to 1.0 the attribute value. Values above 0.66 describe tracks that are probably made entirely of spoken words. Values between 0.33 and 0.66 describe tracks that may contain both music and speech, either in sections or layered, including such cases as rap music. Values below 0.33 most likely represent music and other non-speech-like tracks.</p>
+            <div
+              id="speechiness"
+              v-if="featureDropDown[selectedAf] == 'speechiness'"
+            >
+              <p class="my-4">
+                Speechiness detects the presence of spoken words in a track. The
+                more exclusively speech-like the recording (e.g. talk show,
+                audio book, poetry), the closer to 1.0 the attribute value.
+                Values above 0.66 describe tracks that are probably made
+                entirely of spoken words. Values between 0.33 and 0.66 describe
+                tracks that may contain both music and speech, either in
+                sections or layered, including such cases as rap music. Values
+                below 0.33 most likely represent music and other non-speech-like
+                tracks.
+              </p>
             </div>
-            <div id="acousticness" v-if="featureDropDown[selectedAf] == 'acousticness'">
-              <p
-                class="my-4"
-              >A confidence measure from 0.0 to 1.0 of whether the track is acoustic. 1.0 represents high confidence the track is acoustic.</p>
+            <div
+              id="acousticness"
+              v-if="featureDropDown[selectedAf] == 'acousticness'"
+            >
+              <p class="my-4">
+                A confidence measure from 0.0 to 1.0 of whether the track is
+                acoustic. 1.0 represents high confidence the track is acoustic.
+              </p>
             </div>
             <div id="liveness" v-if="featureDropDown[selectedAf] == 'liveness'">
-              <p
-                class="my-4"
-              >Detects the presence of an audience in the recording. Higher liveness values represent an increased probability that the track was performed live. A value above 0.8 provides strong likelihood that the track is live.</p>
+              <p class="my-4">
+                Detects the presence of an audience in the recording. Higher
+                liveness values represent an increased probability that the
+                track was performed live. A value above 0.8 provides strong
+                likelihood that the track is live.
+              </p>
             </div>
             <div id="valence" v-if="featureDropDown[selectedAf] == 'valence'">
-              <p
-                class="my-4"
-              >A measure from 0.0 to 1.0 describing the musical positiveness conveyed by a track. Tracks with high valence sound more positive (e.g. happy, cheerful, euphoric), while tracks with low valence sound more negative (e.g. sad, depressed, angry).</p>
+              <p class="my-4">
+                A measure from 0.0 to 1.0 describing the musical positiveness
+                conveyed by a track. Tracks with high valence sound more
+                positive (e.g. happy, cheerful, euphoric), while tracks with low
+                valence sound more negative (e.g. sad, depressed, angry).
+              </p>
             </div>
-            <div id="instrumentalness" v-if="featureDropDown[selectedAf] == 'instrumentalness'">
-              <p
-                class="my-4"
-              >Predicts whether a track contains no vocals. “Ooh” and “aah” sounds are treated as instrumental in this context. Rap or spoken word tracks are clearly “vocal”. The closer the instrumentalness value is to 1.0, the greater likelihood the track contains no vocal content. Values above 0.5 are intended to represent instrumental tracks, but confidence is higher as the value approaches 1.0.</p>
+            <div
+              id="instrumentalness"
+              v-if="featureDropDown[selectedAf] == 'instrumentalness'"
+            >
+              <p class="my-4">
+                Predicts whether a track contains no vocals. “Ooh” and “aah”
+                sounds are treated as instrumental in this context. Rap or
+                spoken word tracks are clearly “vocal”. The closer the
+                instrumentalness value is to 1.0, the greater likelihood the
+                track contains no vocal content. Values above 0.5 are intended
+                to represent instrumental tracks, but confidence is higher as
+                the value approaches 1.0.
+              </p>
             </div>
             <div id="tempo" v-if="featureDropDown[selectedAf] == 'tempo'">
-              <p
-                class="my-4"
-              >The overall estimated tempo of a track in beats per minute (BPM). In musical terminology, tempo is the speed or pace of a given piece and derives directly from the average beat duration.</p>
+              <p class="my-4">
+                The overall estimated tempo of a track in beats per minute
+                (BPM). In musical terminology, tempo is the speed or pace of a
+                given piece and derives directly from the average beat duration.
+              </p>
             </div>
           </b-modal>
         </div>
@@ -86,11 +140,16 @@
     <div id="bar-diagram-container" v-if="averageAf.length > 0">
       <div id="average-af-title">
         <p>
-          <b>Average audio features</b>
+          <b>average</b>
         </p>
       </div>
       <div id="bar-diagram">
-        <bars :data="averageAf" :gradient="['#6fa8dc', '#42b983']" :barWidth="40" :growDuration="1"></bars>
+        <bars
+          :data="averageAf"
+          :gradient="['#6fa8dc', '#42b983']"
+          :barWidth="35"
+          :growDuration="1"
+        ></bars>
       </div>
     </div>
     <h4 v-if="authenticationError !== ''">
